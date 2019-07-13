b4157588cfff688cb00a38209161e8c03d4fb8d3
jeremyrudman <jeremyrudman@gmail.com>
2019-07-12 20:19:51 -0400

    a very basic home page to redirect to other projects

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Jeremy Rudman website</h1>
+                <div className="photo"><img  src={require('./photo.jpg')} alt=""/></div>
+                <p>I am a third year computer science student. This is a site to host my web projects.</p>
+                <ul>
+                    <li><a href="./weather">Weather app</a></li>
+                    <li><a href="./ISS">International Space Station tracker</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+
+
+
+ReactDOM.render(<Home />, document.getElementById('root'));
+
